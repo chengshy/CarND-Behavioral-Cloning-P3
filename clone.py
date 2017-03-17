06fe7f6a7b92ed603baeb7ad971ffb866f6056de
chengshy <chengshangyi@gmail.com>
2017-03-16 21:02:16 -0700

    Fisrt simple model

---
+++ clone.py
@@ -0,0 +1,45 @@
+import csv
+import cv2
+import numpy as np
+
+lines = []
+with open('./recorded_data/driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for line in reader:
+        lines.append(line)
+
+images = []
+measurements = []
+for line in lines:
+    source_path = line[0]
+    filename = source_path.split('/')[-1]
+    current_path = './recorded_data/IMG' + filename
+    image = cv2.imread(current_path)
+    measurement = float(line[3])
+    measurements.append(measurement)
+
+augmented_images = []
+augmented_measurements = []
+for image, measurement in zip(images, measurements):
+    augmented_images.append(image)
+    augmented_measurements.append(measurement)
+    augmented_images.append(cv2.flip(image,1))
+    augmented_measurements.append(measurement * -1.0)
+
+X_train = np.array(augmented_images)
+y_train = np.array(augmented_measurements)
+
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
+
+model = Sequential()
+model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape = (160, 320, 3)))
+model.add(Flatten())
+model.add(Dense(1))
+
+model.compile(loss='mse', optimizer='adam')
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
+
+model.save('model.h5')
