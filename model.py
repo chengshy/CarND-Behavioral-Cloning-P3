64c2f8777c704668a4fb8f6c8488e23648a24e0a
chengshy <chengshangyi@gmail.com>
2017-03-23 06:28:33 +0000

    Usable model

--- model.py
+++ model.py
@@ -12,7 +12,7 @@ with open('./recorded_data/driving_log.csv', 'r') as csvfile:
 print('Dataset has %d data' % len(lines))
 
 from sklearn.model_selection import train_test_split
-train_samples, validation_samples = train_test_split(lines, test_size = 0.2)
+train_samples, validation_samples = train_test_split(lines, test_size = 0.1)
 print('Original Train data size is %d' % len(train_samples))
 print('Original Validation data size is %d' % len(validation_samples))
 
@@ -43,15 +43,15 @@ def generator(samples, batch_size = 1024):
                 image_left = cv2.imread(left_path)
                 image_right = cv2.imread(right_path)
 
-i               # Convert to YUV
+                # YUV
                 image_center = cv2.cvtColor(image_center, cv2.COLOR_BGR2YUV)
                 image_left = cv2.cvtColor(image_left, cv2.COLOR_BGR2YUV)
                 image_right = cv2.cvtColor(image_right, cv2.COLOR_BGR2YUV)
+
                 center_images.append(image_center)
                 left_images.append(image_left)
                 right_images.append(image_right)
                 center_measurement = float(batch_sample[3])
-            
                 correction = 0.1 + center_measurement * center_measurement / 0.45;
                 left_measurement = center_measurement + correction
                 right_measurement = center_measurement - correction
@@ -86,7 +86,7 @@ train_generator = generator(train_samples, batch_size = BATCH_SIZE)
 validation_generator = generator(validation_samples, batch_size = BATCH_SIZE)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout, Activation
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
@@ -94,17 +94,21 @@ model = Sequential()
 #Crop
 model.add(Cropping2D(cropping = ((70, 25), (0, 0)), input_shape = (160, 320, 3)))
 #Normalize 
-model.add(Lambda(lambda x: (x / 255.0) - 0.5))
+model.add(Lambda(lambda x: (x/255.0) - 0.5))
 #Nvidia NN
-model.add(Convolution2D(24,5,5, subsample = (2,2), activation = 'relu'))
-model.add(Convolution2D(36,5,5, subsample = (2,2), activation = 'relu'))
-model.add(Convolution2D(48,5,5, subsample = (2,2), activation = 'relu'))
-model.add(Convolution2D(64,3,3, activation = 'relu'))
-model.add(Convolution2D(64,3,3, activation = 'relu'))
+model.add(Convolution2D(24,5,5, subsample = (2,2), activation = 'tanh'))
+model.add(Convolution2D(36,5,5, subsample = (2,2), activation = 'tanh'))
+model.add(Convolution2D(48,5,5, subsample = (2,2), activation = 'tanh'))
+model.add(Convolution2D(64,3,3, activation = 'tanh'))
+model.add(Convolution2D(64,3,3, activation = 'tanh'))
 model.add(Flatten())
-model.add(Dense(100))
-model.add(Dense(50))
-model.add(Dense(10))
+model.add(Activation('tanh'))
+model.add(Dropout(0.3))
+model.add(Dense(100, activation = 'tanh'))
+model.add(Dropout(0.1))
+model.add(Dense(50, activation = 'tanh'))
+model.add(Dropout(0.2))
+model.add(Dense(10, activation = 'tanh'))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
