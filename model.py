1a1e8f59b892dc8e729fba907e911104d0f70511
chengshy <chengshangyi@gmail.com>
2017-04-01 21:18:38 +0000

    Finalize the model

--- model.py
+++ model.py
@@ -3,11 +3,14 @@ import cv2
 import numpy as np
 import sklearn
 
-def get_filenames(path_prefix, log_name):
+def get_filenames(path_prefix, log_name, random_drop_small_angle = True):
     lines = []
     with open(path_prefix + log_name, 'r') as csvfile:
         reader = csv.reader(csvfile)
         for line in reader:
+            if abs(float(line[3])) < 0.05:
+                if np.random.randint(100) < 65:
+                    continue
             lines.append(line)
     print('%s Dataset has %d data' % (log_name, len(lines)) )
     return lines
@@ -15,13 +18,12 @@ def get_filenames(path_prefix, log_name):
 path_prefix = './all_data/'
 lines_turn = get_filenames(path_prefix, 'driving_log_turn.csv')
 lines_first = get_filenames(path_prefix, 'driving_log_first_track.csv')
-sklearn.utils.shuffle(lines_first)
-lines_first = lines_first[: int(len(lines_first) / 4)]
 lines_second = get_filenames(path_prefix, 'driving_log_second_track.csv')
 lines_recovery = get_filenames(path_prefix, 'driving_log_recovery.csv')
+lines_carnd = get_filenames(path_prefix, 'driving_log_carnd.csv')
 
 lines = []
-lines += lines_turn + lines_first + lines_second + lines_recovery
+lines += lines_turn + lines_carnd + lines_recovery + lines_first + lines_second
 print('Dataset has %d data' % len(lines))
 
 from sklearn.model_selection import train_test_split
@@ -30,7 +32,7 @@ train_samples, validation_samples = train_test_split(lines, test_size = 0.2)
 print('Original Train data size is %d' % len(train_samples))
 print('Original Validation data size is %d' % len(validation_samples))
 
-def generator(samples, batch_size = 1024, use_left_right = False):
+def generator(samples, path_prefix, batch_size = 1024, use_left_right = False):
     num_samples = len(samples)
     while 1:
         sklearn.utils.shuffle(samples)
@@ -50,7 +52,7 @@ def generator(samples, batch_size = 1024, use_left_right = False):
                 filename_center = center_path.split('/')[-1]
                 filename_left = left_path.split('/')[-1]
                 filename_right = right_path.split('/')[-1]
-                center_path = './all_data/IMG/' + filename_center
+                center_path = path_prefix + 'IMG/' + filename_center
                 image_center = cv2.imread(center_path)
                 image_center = cv2.cvtColor(image_center, cv2.COLOR_BGR2YUV)
                 center_images.append(image_center)
@@ -58,15 +60,16 @@ def generator(samples, batch_size = 1024, use_left_right = False):
                 center_measurements.append(center_measurement)
 
                 if use_left_right:
-                    left_path = './all_data/IMG/' + filename_left
-                    right_path = './all_data/IMG/' + filename_right
+                    left_path = path_prefix + 'IMG/' + filename_left
+                    right_path = path_prefix + 'IMG/' + filename_right
                     image_left = cv2.imread(left_path)
                     image_right = cv2.imread(right_path)
                     image_left = cv2.cvtColor(image_left, cv2.COLOR_BGR2YUV)
                     image_right = cv2.cvtColor(image_right, cv2.COLOR_BGR2YUV)
                     left_images.append(image_left)
                     right_images.append(image_right)
-                    correction = 0.2 + center_measurement * center_measurement / 0.45;
+                    # correction = 0.175 + center_measurement * center_measurement / 0.45;
+                    correction = 0.23;
                     left_measurement = center_measurement + correction
                     right_measurement = center_measurement - correction
                     left_measurements.append(left_measurement)
@@ -93,16 +96,17 @@ def generator(samples, batch_size = 1024, use_left_right = False):
             y_train = np.array(augmented_measurements)
             yield sklearn.utils.shuffle(X_train, y_train)
 
-BATCH_SIZE  = 128
+BATCH_SIZE  = 64
 use_left_right = True
-train_generator = generator(train_samples, batch_size = BATCH_SIZE, use_left_right = use_left_right)
-validation_generator = generator(validation_samples, batch_size = BATCH_SIZE, use_left_right = use_left_right)
+train_generator = generator(train_samples, path_prefix, batch_size = BATCH_SIZE, use_left_right = use_left_right)
+validation_generator = generator(validation_samples, path_prefix, batch_size = BATCH_SIZE, use_left_right = use_left_right)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout, Activation
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout, Activation, SpatialDropout2D
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 from keras.callbacks import ModelCheckpoint, EarlyStopping
+from keras.optimizers import Adam
 
 model = Sequential()
 #Crop
@@ -110,33 +114,36 @@ model.add(Cropping2D(cropping = ((55, 25), (0, 0)), input_shape = (160, 320, 3))
 #Normalize 
 model.add(Lambda(lambda x: (x / 255.0) - 0.5))
 #Nvidia NN
-model.add(Convolution2D(24,5,5, subsample = (2,2), activation = 'tanh'))
-model.add(Convolution2D(36,5,5, subsample = (2,2), activation = 'tanh'))
-model.add(Convolution2D(48,5,5, subsample = (2,2), activation = 'tanh'))
-model.add(Convolution2D(64,3,3, activation = 'tanh'))
-model.add(Convolution2D(64,3,3, activation = 'tanh'))
+model.add(Convolution2D(24,5,5, subsample = (2,2), activation = 'elu'))
+model.add(SpatialDropout2D(0.2))
+model.add(Convolution2D(36,5,5, subsample = (2,2), activation = 'elu'))
+model.add(SpatialDropout2D(0.2))
+model.add(Convolution2D(48,5,5, subsample = (2,2), activation = 'elu'))
+model.add(SpatialDropout2D(0.2))
+model.add(Convolution2D(64,3,3, activation = 'elu'))
+model.add(SpatialDropout2D(0.2))
+model.add(Convolution2D(64,3,3, activation = 'elu'))
 model.add(Flatten())
-model.add(Activation('tanh'))
 model.add(Dropout(0.5))
-model.add(Dense(100, activation = 'tanh'))
-model.add(Dropout(0.2))
-model.add(Dense(50, activation = 'tanh'))
-model.add(Dropout(0.2))
-model.add(Dense(10, activation = 'tanh'))
+model.add(Dense(100, activation = 'elu'))
+model.add(Dense(50, activation = 'elu'))
+model.add(Dense(10, activation = 'elu'))
+model.add(Dropout(0.5))
 model.add(Dense(1))
 
-model.compile(loss='mse', optimizer='adam')
+model.compile(loss='mse', optimizer=Adam(lr = 0.001))
 
 checkpointer = ModelCheckpoint(filepath="/tmp/weights.h5", verbose=1, save_best_only=True)
 
-earlystop = EarlyStopping()
+earlystop = EarlyStopping(patience = 1)
 
 samples_num = 6 * len(train_samples) if use_left_right else 2 * len(train_samples)
+validation_num = 6 * len(validation_samples) if use_left_right else 2 * len(validation_samples)
 
 model.fit_generator(train_generator, samples_per_epoch = samples_num,
     validation_data = validation_generator, 
-    nb_val_samples = len(validation_samples), nb_epoch = 5,
+    nb_val_samples = validation_num, nb_epoch = 50,
     verbose = 1,
-    callbacks = [earlystop, checkpointer])
+    callbacks = [checkpointer, earlystop])
 
 model.save('model.h5')
